0334dc73f25630b29a68fbe944ada3e1906f6403
Kevin French <fnivek@gmail.com>
2015-10-18 22:01:03 -0400

    QUEUE: Add queue functionality

--- main.c
+++ main.c
@@ -1,8 +1,66 @@
 #include <msp430.h>
 #include <stdint.h>
+#include <limits.h>
 
 #define RX BIT1
 #define TX BIT2
+// Must be less than max(unsigned int)
+#define MAX_TX_BUF_SIZE 100
+
+typedef struct queue {
+	uint8_t elements[MAX_TX_BUF_SIZE];
+	unsigned int front;
+	unsigned int count;
+} queue;
+
+// Pops an element off a queue and places its value in value
+// returns 1 for success and 0 for underflow
+uint8_t QueuePop(queue* q, uint8_t* value) {
+	// Check for underflow
+	if (!q->count) return 0;
+
+	// Pop the value off
+	--q->count;
+	(*value) = q->elements[q->front--];
+
+	// Wrap front if necisary
+	if (q->front >= MAX_TX_BUF_SIZE) {
+		q->front = MAX_TX_BUF_SIZE - 1;
+	}
+
+	// Success
+	return 1;
+}
+
+// Return the current size of the queue
+uint8_t QueueSize(queue *q) {
+	return q->count;
+}
+
+// Insert a new value into the queue
+// Return 1 for success 0 for QueueFull
+uint8_t QueueInsert(queue* q, uint8_t value) {
+	// Check if full
+	if(q->count >= MAX_TX_BUF_SIZE) return 0;
+
+	unsigned int offset = q->count + q->front;
+	// Check if need to wrap to front
+	if(offset >= MAX_TX_BUF_SIZE)
+	{
+		offset -= MAX_TX_BUF_SIZE;
+	}
+	else if(offset < q->front && MAX_TX_BUF_SIZE < UINT_MAX)
+	{
+		offset += UINT_MAX - MAX_TX_BUF_SIZE;
+	}
+
+	q->elements[offset] = value;
+	++q->count;
+	return 1;
+}
+
+queue UARTTXBuf;
+queue SPITXBuf;
 
 // Transfer one byte over SPI
 void SPITransmit(uint8_t out) {
@@ -11,11 +69,16 @@ void SPITransmit(uint8_t out) {
 
 // Transfer one byte over UART
 void UARTTransmit(uint8_t out) {
-	// Wait for transmit to be ready
-	while (!(IFG2 & UCA0TXIFG));
+	if (!QueueInsert(&UARTTXBuf, out)) {
+		// Handle Queue overflow
+		__disable_interrupt();
+		for (;;); // This is here to intentionally break the code
+	}
 
-	UCA0TXBUF = out;
-	__delay_cycles(100000);
+	// Turn the transmit interrupt back on
+	if(QueueSize(&UARTTXBuf) == 1) {
+		IE2 |= UCA0TXIE;
+	}
 }
 
 // Initilize UART on UCA0
@@ -45,6 +108,23 @@ void InitUART() {
 
 }
 
+#pragma vector=USCIAB0TX_VECTOR
+__interrupt void TXISR() {
+	uint8_t out = 0;
+	if(QueuePop(&UARTTXBuf, &out)) {
+		UCA0TXBUF = out;
+	}
+	else {
+		// Done transfering disable transmit interrupt
+		IE2 &= ~UCA0TXIE;
+	}
+}
+
+#pragma vector=USCIAB0RX_VECTOR
+__interrupt void RXISR() {
+	UARTTransmit(UCA0RXBUF);
+}
+
 /*
  * main.c
  */
@@ -58,14 +138,18 @@ int main(void) {
 	// Initilize UART
 	InitUART();
 
-	while(!(IFG2 & UCA0RXIFG));
+	// Enable interrupts
+	__enable_interrupt();
+	IE2 = UCA0TXIE | UCA0RXIE;
 
-	uint8_t echo = UCA0RXBUF;
+	//while(!(IFG2 & UCA0RXIFG));
+
+	//uint8_t echo = UCA0RXBUF;
 
 	while(1) {
-		UARTTransmit(echo);
-		while(!(IFG2 & UCA0RXIFG));
-		echo = UCA0RXBUF;
+		//UARTTransmit(echo);
+		//while(!(IFG2 & UCA0RXIFG));
+		//echo = UCA0RXBUF;
 	}
 
 	return 0;
